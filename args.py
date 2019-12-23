98b71ce58677ce49d7d98ff336170a95563690e4
jackkang0 <55919881+jackkang0@users.noreply.github.com>
2019-12-23 09:36:05 +0800

    抓日期

--- args.py
+++ args.py
@@ -6,5 +6,12 @@ def get_args():
     # TODO: Add --start-date, --end-date and --output arguments
     #       Convert the two dates to datetime objects
     parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--start-date', required = True)
+    parser.add_argument('-e', '--end-date', required = True)
+    parser.add_argument('-o', '--output', default = 'output.csv')
+
     args = parser.parse_args()
+    args.start_date = datetime.strptime(args.start_date, "%Y-%m-%d")
+    args.end_date = datetime.strptime(args.end_date, "%Y-%m-%d")
+    print(args)
     return args
